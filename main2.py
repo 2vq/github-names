b3ffa63856600a9ca8b8e735957568c577fbee3a
6vi <74454528+6vi@users.noreply.github.com>
2021-12-12 16:33:45 -0500

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,41 @@
+import os
+import time
+import random
+import string
+import requests
+import threading
+from slowprint.slowprint import slowprint as slowp
+from colorama import Fore
+
+os.system('cls')
+
+#threadcount = input(f'{Fore.LIGHTBLACK_EX}>{Fore.BLUE} Threads{Fore.LIGHTBLACK_EX}: {Fore.LIGHTBLUE_EX}')
+#amount = input(f'{Fore.LIGHTBLACK_EX}>{Fore.BLUE} Amount{Fore.LIGHTBLACK_EX}: {Fore.LIGHTBLUE_EX}')
+#Letter = input(f'{Fore.LIGHTBLACK_EX}>{Fore.BLUE} Letters{Fore.LIGHTBLACK_EX}: {Fore.LIGHTBLUE_EX}')
+
+def check():
+  slowp(f'NOTE: This wont get you ratelimited so easily', 0.2)
+    amount = input(f'{Fore.LIGHTBLACK_EX}>{Fore.BLUE} Amount{Fore.LIGHTBLACK_EX}: {Fore.LIGHTBLUE_EX}')
+    Letter = input(f'{Fore.LIGHTBLACK_EX}>{Fore.BLUE} Letters{Fore.LIGHTBLACK_EX}: {Fore.LIGHTBLUE_EX}')
+    for i in range(int(amount)):
+        try:
+            code = ''.join(random.choices(string.ascii_letters + string.digits, k = (int(Letter))))
+            r = requests.get(f'https://github.com/{code}')
+            if r.status_code == 404:
+                print(f'{Fore.LIGHTBLACK_EX}>{Fore.BLUE} Non Taken Name{Fore.LIGHTBLACK_EX}: {Fore.LIGHTBLUE_EX}{code}')
+                with open('github-names.txt', 'a', encoding = 'UTF-8') as f:
+                    f.write(f'https://github.com/{code}\n')
+            elif r.status_code == 200:
+                print(f'{Fore.LIGHTBLACK_EX}>{Fore.RED} Taken Name{Fore.LIGHTBLACK_EX}: {Fore.LIGHTRED_EX}{code}')
+            else:
+                pass
+        except:
+            pass
+
+#def thread():
+#    for i in range(int(threadcount)):
+#        threading.Thread(target = check).start()
+    
+if __name__ == '__main__':
+    #thread()
+    check()
